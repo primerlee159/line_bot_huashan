2b312cbf90d473090a4fa0593e625685a5220d8d
primerlee <leo2254367642@gmail.com>
2024-05-26 20:09:08 +0800

    test message sending

--- app.py
+++ app.py
@@ -34,9 +34,10 @@ def callback():
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    # message = TextSendMessage(text=event.message.text)
-    message = 'get'
-    line_bot_api.reply_message(event.reply_token, message)
+    line_bot_api.reply_message(
+        event.reply_token, 
+        TextSendMessage(text="收到")
+        )
 
 import os
 if __name__ == "__main__":
