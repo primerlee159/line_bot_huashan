90b52691529a9a67c8b660225a51dca60730621c
primerlee <leo2254367642@gmail.com>
2024-05-26 20:03:40 +0800

    test chn bug

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     # message = TextSendMessage(text=event.message.text)
-    message = "收到"
+    message = 'get'
     line_bot_api.reply_message(event.reply_token, message)
 
 import os
